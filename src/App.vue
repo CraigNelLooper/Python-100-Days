2dda52a94b1f710136113c40fab21936f2e2362d
CraigNelLooper <craig@looper.net.za>
2022-03-23 15:44:17 +0200

    finishing off the main page

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,15 @@
     <v-app-bar app color="white" flat>
       <v-container class="py-0 fill-height">
         <v-app-bar-nav-icon class="hidden-sm-and-up"></v-app-bar-nav-icon>
-        <v-toolbar-title class="mr-4">Daskrans</v-toolbar-title>
+        <v-img class="mr-2" contain max-height="64px" max-width="64px" src="@/assets/1.png"></v-img>
+        <v-toolbar-title class="mr-4" style="font-family:'Cinzel', serif; font-size:24px ; font-weight:700"> Daskrans</v-toolbar-title>
         <v-spacer></v-spacer>
         <div class="hidden-sm-and-down">
-        <v-btn color="primary" v-for="link in links" :key="link" text>
-          {{ link }}
-        </v-btn>
+          <v-btn color="primary" v-for="link in links" :key="link" text>
+            {{ link }}
+          </v-btn>
 
-        <v-btn plain tile elevation="2" color="secondary">Aetera Token</v-btn>
+          <v-btn disabled tile elevation="2" color="secondary">Aetera Token</v-btn>
         </div>
       </v-container>
     </v-app-bar>
@@ -18,13 +19,34 @@
     <v-main class="grey lighten-3">
       <router-view />
     </v-main>
+    <v-footer  padless>
+      <v-card flat tile width="100%" class="blue lighten-1 text-center">
+        <v-card-text>
+          <v-btn v-for="icon in icons" :key="icon" class="mx-4" icon>
+            <v-icon size="24px">
+              {{ icon }}
+            </v-icon>
+          </v-btn>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>Daskrans | Spring Water</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
 export default {
   data: () => ({
-    links: ["About Us", "Where to buy", "Contact us", "The People"],
+    links: ["About Us", "Where to buy",  "The People", "Contact us",],
   }),
 };
 </script>
+<style scoped>
+@import url(https://fonts.googleapis.com/css2?family=Cinzel&display=swap);
+
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,113 @@
 <template>
-  <div fluid>
+  <div fluid style="background-color:white">
     <section>
-      <v-carousel cycle height="76vh">
+      <div class="header">
+        <!--Content before waves-->
+        <div
+          class="inner-header"
+          style="align-items: center; justify-content: center; display: flex "
+        >
+          <!--Just the logo.. Don't mind this-->
+          <v-rows >
+            <v-col  cols="12">
+              <v-img class="mx-auto" max-width="300px" src="@/assets/1white.png"></v-img>
+            </v-col>
+            <v-col cols="12">
+              <h1>Daskrans Spring Water</h1>
+            </v-col>
+            <v-col cols="12">
+              <p>
+                The life blood of water, straight from the earth, as it was
+                meant to be
+              </p>
+            </v-col>
+          </v-rows>
+        </div>
+
+        <!--Waves Container-->
+        <div>
+          <svg
+            class="waves"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            viewBox="0 24 150 28"
+            preserveAspectRatio="none"
+            shape-rendering="auto"
+          >
+            <defs>
+              <path
+                id="gentle-wave"
+                d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+              />
+            </defs>
+            <g class="parallax">
+              <use
+                xlink:href="#gentle-wave"
+                x="48"
+                y="0"
+                fill="rgba(255,255,255,0.7"
+              />
+              <use
+                xlink:href="#gentle-wave"
+                x="48"
+                y="3"
+                fill="rgba(255,255,255,0.5)"
+              />
+              <use
+                xlink:href="#gentle-wave"
+                x="48"
+                y="5"
+                fill="rgba(255,255,255,0.3)"
+              />
+              <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
+            </g>
+          </svg>
+        </div>
+        <!--Waves end-->
+      </div>
+      <!--Header ends-->
+
+      <!--Content ends-->
+    </section>
+    <section>
+      <div class="text-center mt-4"><h1 style="font-size : 48px">Photos from the ground</h1></div>
+      <v-container>
+      <v-carousel style="border-radius:8px" cycle height="76vh">
         <v-carousel-item>
           <v-sheet height="100%">
-            <v-row class="fill-height" align="center" justify="center"> 
-              <v-img src="https://images.unsplash.com/photo-1564401003800-3cd8ced29bbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80">
-            <v-row align="center" justify="center"> 
-            We take water seriously              
-            </v-row>
+            <v-row class="fill-height" align="center" justify="center">
+              <v-img
+                src="https://images.unsplash.com/photo-1564401003800-3cd8ced29bbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
+              >
               </v-img>
-              
             </v-row>
           </v-sheet>
         </v-carousel-item>
         <v-carousel-item>
           <v-sheet height="100%">
-            <v-row class="fill-height" align="center" justify="center"> 
-              <v-img src="https://images.unsplash.com/photo-1564401003800-3cd8ced29bbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"></v-img>
-              
+            <v-row class="fill-height" align="center" justify="center">
+              <v-img
+                src="https://images.unsplash.com/photo-1564401003800-3cd8ced29bbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
+              ></v-img>
             </v-row>
           </v-sheet>
         </v-carousel-item>
         <v-carousel-item>
           <v-sheet height="100%">
-            <v-row class="fill-height" align="center" justify="center"> 
-              <v-img src="https://images.unsplash.com/photo-1564401003800-3cd8ced29bbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"></v-img>
-              
+            <v-row class="fill-height" align="center" justify="center">
+              <v-img
+                src="https://images.unsplash.com/photo-1564401003800-3cd8ced29bbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
+              ></v-img>
             </v-row>
           </v-sheet>
         </v-carousel-item>
       </v-carousel>
+      </v-container>
     </section>
     <v-container style="max-width: 80vw">
       <v-row class="text-right" align="center">
         <v-col cols="12" md="6">
-          <h1>Welcome to Daskrans Spring Water</h1>
+          <h1 style="font-size:36px">Welcome to Daskrans Spring Water</h1>
           <p>
             We understand the importance of clean, mineral rich water and we
             have decided to share that with the world in the form of Daskrans
@@ -53,6 +125,7 @@
         </v-col>
         <v-col cols="12" md="6">
           <v-img
+            eager
             style="border-radius: 8px"
             src="https://images.unsplash.com/photo-1631472548473-3e1a1c74baeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
           ></v-img>
@@ -60,15 +133,19 @@
       </v-row>
     </v-container>
     <v-container class="mt-4" style="max-width: 80vw">
-      <v-row  align="center">
+      <v-row align="center">
         <v-col cols="12" md="6">
           <v-img
+            eager
             style="border-radius: 8px"
             src="https://images.unsplash.com/photo-1456440234190-5ace46584c7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
           ></v-img>
         </v-col>
         <v-col cols="12" md="6">
-          <h1>The future leader in spring water combined with technology</h1>
+          <h1 style="font-size:36px">
+            The future leader in spring water combined with decentralised
+            technology
+          </h1>
           <p>
             We understand the importance of clean, mineral rich water and we
             have decided to share that with the world in the form of Daskrans
@@ -147,3 +224,112 @@ export default {
   }),
 };
 </script>
+<style scoped>
+@import url(https://fonts.googleapis.com/css2?family=Cinzel&display=swap);
+@import url(https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap);
+
+body {
+  margin: 0;
+}
+p {
+  font-family: "Comfortaa", cursive;
+}
+h1 {
+  font-family: "Cinzel", serif;
+  font-weight: 300;
+  letter-spacing: 2px;
+  font-size: 64px;
+}
+
+.header {
+  position: relative;
+  text-align: center;
+  background: linear-gradient(
+    60deg,
+    rgba(3, 79, 160, 1) 0%,
+    rgba(0, 163, 245, 1) 100%
+  );
+  color: white;
+}
+.logo {
+  width: 50px;
+  fill: white;
+  padding-right: 15px;
+  display: inline-block;
+  vertical-align: middle;
+}
+
+.inner-header {
+  height: 60vh;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+.flex {
+  /*Flexbox for containers*/
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.waves {
+  position: relative;
+  width: 100%;
+  height: 15vh;
+  margin-bottom: -7px; /*Fix for safari gap*/
+  min-height: 100px;
+  max-height: 150px;
+}
+
+.content {
+  position: relative;
+  height: 20vh;
+  text-align: center;
+  background-color: white;
+}
+
+/* Animation */
+
+.parallax > use {
+  animation: move-forever 25s cubic-bezier(0.55, 0.5, 0.45, 0.5) infinite;
+}
+.parallax > use:nth-child(1) {
+  animation-delay: -2s;
+  animation-duration: 7s;
+}
+.parallax > use:nth-child(2) {
+  animation-delay: -3s;
+  animation-duration: 10s;
+}
+.parallax > use:nth-child(3) {
+  animation-delay: -4s;
+  animation-duration: 13s;
+}
+.parallax > use:nth-child(4) {
+  animation-delay: -5s;
+  animation-duration: 20s;
+}
+@keyframes move-forever {
+  0% {
+    transform: translate3d(-90px, 0, 0);
+  }
+  100% {
+    transform: translate3d(85px, 0, 0);
+  }
+}
+/*Shrinking for mobile*/
+@media (max-width: 768px) {
+  .waves {
+    height: 40px;
+    min-height: 40px;
+  }
+  .content {
+    height: 30vh;
+  }
+  h1 {
+    font-size: 24px;
+  }
+}
+</style>
